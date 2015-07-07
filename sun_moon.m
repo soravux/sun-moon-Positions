1efef581bb83e4d759a41fb3031a5025e880408a
Yannick Hold <yannickhold@gmail.com>
2015-07-07 09:19:10 -0400

    Modified script to give the plot of the max gain of the sun+moon

--- sun_moon.m
+++ sun_moon.m
@@ -10,23 +10,28 @@ Interval_minutes = 30;
 % CMU campus (40.443181, -79.943060)
 % defaults to ULaval campus (46.779077, -71.275778)
 location.latitude = 46.779077;
-location.longitude = -71.275778; 
+location.longitude = -71.275778;
 location.altitude = 0;
 UTC = -4;
 
-figure('Position',[100 100 600 600],'Color','white');
+% set to 15 for full moon
+desiredLunarPhase = 1:30;
 
-for i_lat = 60:-10:-60 
-    % Caculate the sun moon positions for the most places around the world
+%latitudes = 60:-10:-60;
+latitudes = location.latitude;
+
+%figure('Position',[100 100 600 600],'Color','white');
+
+for i_lat = latitudes
     location.latitude = i_lat; % longitude controls the time in this code
 
-    % Filter by Lunar Calender
-    %   we only watch the moon in full prase 
+    % Filter by Lunar Calendar
+    %   we only watch the moon in full phase
     start_date = datestr(start_time,'yyyymmdd');
     end_date = datestr(end_time,'yyyymmdd');
     oneDayNum = datenum('02','dd') - datenum('01','dd');
     dayNb = (datenum(end_date,'yyyymmdd')-datenum(start_date,'yyyymmdd'))/oneDayNum;
-    i_cnt = 0;
+    desiredDay = {}; lunarPhases = {};
     
     for i_d = 1:dayNb
         
@@ -34,22 +39,25 @@ for i_lat = 60:-10:-60
         yyyy = curDay(1:4);mm=curDay(5:6);dd=curDay(7:8);
         lunarDay = LunarCalendar(yyyy,mm,dd); lunarDay = strsplit(lunarDay,':');lunarDay = lunarDay{3};    
         
-        % Full Moon is 15th in lunna calendar
-        if strcmp(lunarDay,'LD15')
-            i_cnt = i_cnt + 1;
-            FullMoonList{i_cnt} = curDay;        
+        % Only select desired days
+        lunarPhase = str2double(lunarDay(3:end));
+        if ismember(lunarPhase, desiredLunarPhase)
+            desiredDay{end + 1} = curDay;
+            lunarPhases{end + 1} = lunarPhase;
         end
         
     end
 
     % Organize the date
-    dateNumbers = length(FullMoonList);
+    dateNumbers = length(desiredDay);
     timenumStep = Interval_minutes * (datenum('02','MM')-datenum('01','MM'));
     timenum24hour = 1;
+    
+    lambda_max = zeros(1, dateNumbers);
 
     % Compute the sun and moon position for each Full Moon Day
     for i_d = 1:dateNumbers
-        curFullMoonDate = FullMoonList{i_d};
+        curFullMoonDate = desiredDay{i_d};
         
         % Add time for this full moon day
         start_datetime = '08:00:00';
@@ -67,7 +75,7 @@ for i_lat = 60:-10:-60
                      'hour', h, 'min', mn, 'sec', floor(s), 'UTC', UTC);
 
             angle = moonPosition(time,location);
-            
+
             if angle.zenith > 0 
                 moon_cnt = moon_cnt + 1;
                 moon.position(moon_cnt,:) = [angle.azimuth,angle.zenith];
@@ -75,9 +83,9 @@ for i_lat = 60:-10:-60
                 moon.normal(moon_cnt,:) = column([x,y,z]);
                 moon.time(moon_cnt,:) = time;
             end
-            
+
             angle = sunPosition(time,location);
-            
+
             if angle.zenith > 0
                 sun_cnt = sun_cnt + 1;
                 sun.position(sun_cnt,:) = [angle.azimuth,angle.zenith];
@@ -88,9 +96,6 @@ for i_lat = 60:-10:-60
             
         end
 
-
-        visibility_sun = zeros(1,size(sun.normal,1));
-        visibility_moon = zeros(1,size(moon.normal,1));
         visibility_sun = sun.normal(:,3)>0;
         visibility_moon = moon.normal(:,3)>0;
         visibility_moon = visibility_moon & ~visibility_sun;
@@ -105,39 +110,56 @@ for i_lat = 60:-10:-60
         sun_plane = atan( (max_sunz-min_sunz)/(max_sunx-min_sunx))/pi*180;
         moon_plane = atan( (max_moonz-min_moonz)/(max_moonx-min_moonx))/pi*180;
 
-        %% draw the result for this full moon day
-        clf;
-        fontsize = 14;
-        % draw sun
-        x = sun.normal(:,1); y = sun.normal(:,2); z = sun.normal(:,3);
-        x = x(visibility_sun);y = y(visibility_sun);z = z(visibility_sun);
-        plot3(x,y,z,'ro');  hold on;
-        % draw moon
-        x = moon.normal(:,1); y = moon.normal(:,2); z = moon.normal(:,3);
-        x = x(visibility_moon);y = y(visibility_moon);z = z(visibility_moon);
-        plot3(x,y,z,'b*');
-        % draw sphere
-        [x,y,z] = sphere(50);
-        lightGrey = 0.8*[1 1 1]; % It looks better if the lines are lighter
-        surface(x,y,z,'FaceColor', 'none','EdgeColor',lightGrey)
-        % draw horizon
-        plot3(x((z==0)),y((z==0)),z((z==0)),'black');
+%         %% draw the result for this full moon day
+%         clf;
+%         fontsize = 14;
+%         % draw sun
+%         x = sun.normal(:,1); y = sun.normal(:,2); z = sun.normal(:,3);
+%         x = x(visibility_sun);y = y(visibility_sun);z = z(visibility_sun);
+%         plot3(x,y,z,'ro');  hold on;
+%         % draw moon
+%         x = moon.normal(:,1); y = moon.normal(:,2); z = moon.normal(:,3);
+%         x = x(visibility_moon);y = y(visibility_moon);z = z(visibility_moon);
+%         plot3(x,y,z,'b*');
+%         % draw sphere
+%         [x,y,z] = sphere(50);
+%         lightGrey = 0.8*[1 1 1]; % It looks better if the lines are lighter
+%         surface(x,y,z,'FaceColor', 'none','EdgeColor',lightGrey)
+%         % draw horizon
+%         plot3(x((z==0)),y((z==0)),z((z==0)),'black');
+%         
+%         % Add labels
+%         %xlabel('x');ylabel('y');zlabel('z')
+%         title(sprintf('sun moon position in %s location.lat:%02.f',curFullMoonDate,location.latitude),'FontSize',fontsize);
+%         text(0,-1.3,0,'east','fontsize',fontsize);text(0,1.3,0,'west','FontSize',fontsize)
+%         h=legend(sprintf('sun %02.f',[]),sprintf('full moon %02.f',[]),'Location','southeast');
+%         set(h,'Fontsize',fontsize);
+%         axis off equal
+%         
+%         if location.latitude >=0
+%             view(-70,25);
+%         else
+%             view(70,25);
+%         end
+% 
+%         export_fig(sprintf('%s_lat%02.f.png',curFullMoonDate,location.latitude));
         
-        % Add labels
-        % xlabel('x');ylabel('y');zlabel('z')
-        title(sprintf('sun moon position in %s location.lat:%02.f',curFullMoonDate,location.latitude),'FontSize',fontsize);
-        text(0,-1.3,0,'east','fontsize',fontsize);text(0,1.3,0,'west','FontSize',fontsize)
-        h=legend(sprintf('sun %02.f',[]),sprintf('full moon %02.f',[]),'Location','southeast');
-        set(h,'Fontsize',fontsize);
-        axis off equal
-        
-        if location.latitude >=0
-            view(-70,25);
-        else
-            view(70,25);
-        end
-
-        %saveas(gcf,sprintf('lat%02.f_%s.jpg',location.latitude,curFullMoonDate));
-        export_fig(sprintf('%s_lat%02.f.jpg',curFullMoonDate,location.latitude));
+        L = [sun.normal; moon.normal];
+        lambda_max(i_d) = max(svd(pinv(L')));
     end
+    figure;
+    cm = parula(16);
+    colormap(cm);
+    colors = cm(abs(15 - cell2mat(lunarPhases))+1,:);
+    scatter(1:length(lambda_max), lambda_max, 1, colors);
+    cb = colorbar;
+    title(cb,'Lunar phase')
+    hold on;
+    for i = 1:length(lambda_max) - 1
+        plot([i i+1], lambda_max(i:i+1), 'Color', colors(i,:));
+    end
+    hold off;
+    xlabel('Day');
+    ylabel('Maximum gain');
+    export_fig('maximum_gain.pdf', '-transparent')
 end
